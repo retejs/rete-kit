e77fae43646d67ebbebc80820229a83a0598e6ce
Ni55aN <huccah@gmail.com>
2023-02-19 01:42:04 +0200

    react builder: replace App.tsx and styles

--- src/app/react/index.ts
+++ src/app/react/index.ts
@@ -3,24 +3,23 @@ import execa from 'execa'
 import fs from 'fs'
 import { join } from 'path'
 
-import { assets } from '../../consts'
+import { assets as assetsRoot } from '../../consts'
 import { AppBuilder } from '../app-builder'
 
 export class ReactBuilder implements AppBuilder {
   public name = 'React.js'
   public versions = [16, 17, 18]
 
-  // eslint-disable-next-line max-statements
   public async create(name: string, version: number) {
+    const assets = join(assetsRoot, 'app', 'react')
+    const src = join(name, 'src')
+
     await execa('npx', ['create-react-app', '--template', 'typescript', name], { stdio: 'inherit' })
     await execa('npm', ['--prefix', name, 'i', `react@${version}`, `react-dom@${version}`], { stdio: 'inherit' })
 
-    if (version < 18) {
-      const assetIndexScript = join(assets, 'app', 'react', 'index_tsx')
-      const appIndexScript = join(name, 'src', 'index.tsx')
-
-      await fs.promises.copyFile(assetIndexScript, appIndexScript)
-    }
+    if (version < 18) await fs.promises.copyFile(join(assets, 'index_tsx'), join(src, 'index.tsx'))
+    await fs.promises.copyFile(join(assets, 'rete_css'), join(src, 'rete.css'))
+    await fs.promises.copyFile(join(assets, 'App_tsx'), join(src, 'App.tsx'))
   }
 
   async putScript(name: string, code: string) {
