2c54c5a10f8d98aff24815bf62a84371effab86b
Ni55aN <huccah@gmail.com>
2023-04-20 13:38:08 +0300

    add tag to error message

--- src/shared/throw.ts
+++ src/shared/throw.ts
@@ -1,6 +1,9 @@
 import chalk from 'chalk'
 
 export function throwError(message: string) {
-  console.warn(chalk.red(message.replace(/( {1,})/g, ' ').replace(/^ /, '')))
+  console.warn(
+    '\n', chalk.black.bgRgb(220, 50, 50)(' FAIL '),
+    chalk.red(message.replace(/( {1,})/g, ' ').replace(/^ /, ''))
+  )
   process.exit(1)
 }
