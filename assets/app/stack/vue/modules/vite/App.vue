046197e77acf775f929f3a8dfba44257754c25e5
Ni55aN <huccah@gmail.com>
2023-04-22 17:32:20 +0300

    fix vite styles conflict

--- assets/app/stack/vue/modules/vite/App.vue
+++ assets/app/stack/vue/modules/vite/App.vue
@@ -1,66 +1,67 @@
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import HelloWorld from './components/HelloWorld.vue'
-  import { createEditor } from './rete';
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+import { createEditor } from './rete';
 
-  export default defineComponent({
-    components: {
-      HelloWorld
-    },
-    async mounted() {
-      await createEditor(this.$refs.rete as HTMLElement)
-    }
-  })
-  </script>
+export default defineComponent({
+  components: {
+    HelloWorld
+  },
+  async mounted() {
+    await createEditor(this.$refs.rete as HTMLElement)
+  }
+})
+</script>
 
-  <template>
+<template>
   <!-- <div id="app"> -->
-    <header>
-      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
 
-    <main class="rete" ref="rete"></main>
+  <main class="rete" ref="rete"></main>
   <!-- </div> -->
-  </template>
+</template>
 
-  <style scoped>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.rete {
+  height: 90vh;
+  font-size: 1rem;
+  background: white;
+  border-radius: 1em;
+  text-align: left;
+  border: 3px solid #55b881;
+  line-height: 1;
+}
+
+@media (min-width: 1024px) {
   header {
-    line-height: 1.5;
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
-    display: block;
-    margin: 0 auto 2rem;
+    margin: 0 2rem 0 0;
   }
 
-  .rete {
-    height: 90vh;
-    font-size: 1rem;
-    background: white;
-    border-radius: 1em;
-    text-align: left;
-    border: 3px solid #55b881;
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
-
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-      margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
-  }
-  </style>
+}
+</style>
