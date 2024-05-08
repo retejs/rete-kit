763d4bcd54bc7ade7f26100d09775268ec1ae327
Ni55aN <stoliarov.v.s@gmail.com>
2024-05-08 20:35:10 +0300

    fix(build): exit command on child build exit instead of stderr message

--- src/build/exec.ts
+++ src/build/exec.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import execa from 'execa'
 
-export async function awaitedExec(command: string, args: string[], options: { cwd?: string }, log: (line: string) => void, resolveOn: (line: string) => boolean) {
+type Events = {
+  log: (line: string) => void
+  error: (line: string) => void
+  resolveOn: (line: string) => boolean
+}
+
+export async function awaitedExec(command: string, args: string[], options: { cwd?: string }, events: Events) {
   const execaInstance = execa(command, args, { env: { FORCE_COLOR: 'true' }, ...options })
 
   await new Promise<void>((resolve, reject) => {
@@ -11,14 +17,24 @@ export async function awaitedExec(command: string, args: string[], options: { cw
       if (!str) return
       const lines = str.trim().split('\n')
 
-      lines.forEach(log)
+      lines.forEach(events.log)
 
-      if (lines.some(resolveOn)) resolve()
+      if (lines.some(events.resolveOn)) resolve()
     })
     execaInstance.stderr?.addListener('data', (chunk: Buffer) => {
-      const error = chunk.toString()
+      const str = chunk.toString()
 
-      reject(new Error(error))
+      if (!str) return
+      const lines = str.trim().split('\n')
+
+      lines.forEach(events.error)
+    })
+
+    // reject on exit code !== 0
+    execaInstance.on('exit', code => {
+      if (code !== 0) {
+        reject(new Error(`Command failed with exit code ${code}`))
+      }
     })
   })
 }
